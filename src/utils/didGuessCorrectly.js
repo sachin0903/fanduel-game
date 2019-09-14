640b0a6d10b85d0b43a72ab51658a1938f3457f1
Sachin Sharma <sachinsharma@gamil.com>
2019-09-14 18:49:48 +0100

    updated didyouGuessCorrectly method

--- src/utils/didGuessCorrectly.js
+++ src/utils/didGuessCorrectly.js
@@ -1,8 +1,16 @@
 const didGuessCorrectly = (players, guessedPlayerID, currentPairIDs) => {
+  return players
+      .filter(element => currentPairIDs.includes(element.id))
+      .reduce((accumulator, currentValue) => {
+          return accumulator.id === guessedPlayerID
+            ? accumulator.fppg > currentValue.fppg
+            : currentValue.fppg > accumulator.fppg;
+  });
+/*
   const guessedPlayerFPPG = players.find(({ id }) => guessedPlayerID === id).fppg;
   const otherPlayerID = currentPairIDs.find(id => id !== guessedPlayerID);
   const otherPlayerFPPG = players.find(({ id }) => otherPlayerID === id).fppg;
-  return guessedPlayerFPPG > otherPlayerFPPG;
+  return guessedPlayerFPPG > otherPlayerFPPG;*/
 };
 
 export { didGuessCorrectly };
